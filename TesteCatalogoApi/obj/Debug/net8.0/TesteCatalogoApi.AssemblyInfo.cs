e3e7227d41778d31fa168f0ebd8a59d53d83225d
fabinho1992 <f.santosdev1992@gmail.com>
2024-08-05 18:07:30 -0300

    ultima atualização

--- TesteCatalogoApi/obj/Debug/net8.0/TesteCatalogoApi.AssemblyInfo.cs
+++ TesteCatalogoApi/obj/Debug/net8.0/TesteCatalogoApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TesteCatalogoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8aa22e192d97538d6b17ca0910dd43ef783e5de")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+803b40c4a8372b5085ec4ac94d03b43317abcafc")]
 [assembly: System.Reflection.AssemblyProductAttribute("TesteCatalogoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TesteCatalogoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
